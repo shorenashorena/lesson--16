aaa142864f2f5549dd027c460101b398698bb6db
shorenashorena <shorenabad@gmail.com>
2025-11-14 18:06:33 +0400

    const myArr

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const prices = [10, 20, 30, 40, 50];
 
 console.log(prices)
 
+const firstprice = prices[0];
+
+
+const myArr = [ [1, 2, 3], [4, 5] ];
+
+console.log(myArr[0][1]);  
+
+const myArr2 = [10, 20, (10 + 20) / 2, 10 + 20];
+console.log(myArr2);
+
 
 
 
